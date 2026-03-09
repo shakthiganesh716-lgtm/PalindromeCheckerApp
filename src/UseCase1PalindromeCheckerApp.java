16253ca1cafae07e4345661efc318a6a2be25fd0
shakthiganesh716-lgtm <shakthiganesh716@gmail.com>
2026-03-09 23:14:12 +0530

    UC1: Add welcome message module

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to the Palindrome Checker App");
+        System.out.println("Version : 1.0");
+        System.out.println("=================================");
+
+        System.out.println("System initialized successfully.");
+
+    }
+}
