9f1bcd01320f220a2996905ad691829c315166d5
shakthiganesh716-lgtm <shakthiganesh716@gmail.com>
2026-03-10 00:06:25 +0530

    UC4: Character array based palindrome check

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        // Convert string to character array
+        char[] chars = word.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is Not a Palindrome");
+        }
+
+    }
+}
